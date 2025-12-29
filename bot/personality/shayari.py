3925ccc44d9e6e95f8ed82d5060a051b68397714
CandidVideoEditor <candidvideoeditor@gmail.com>
2025-12-29 08:39:55 +0530

    Add shayari function to return random quotes

---
+++ bot/personality/shayari.py
@@ -0,0 +1,11 @@
+import random
+
+SHAYARI = [
+    "Teri baaton mein kuch toh baat hai ❤️",
+    "Dil se baat nikli hai, dil tak jayegi 💫",
+    "Tu muskura de, duniya haseen lagne lage 💖",
+    "Baatein kam ho, ehsaas zyada ho ❤️"
+]
+
+def get_shayari():
+    return random.choice(SHAYARI)
